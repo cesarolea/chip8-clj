1c87c5fadf01acdfe9b219229373e7013ae64a67
Cesar Olea <cesarolea@gmail.com>
2018-09-23 15:31:57 -0700

    Fix clear display test

--- test/chip8/core_test.clj
+++ test/chip8/core_test.clj
@@ -57,10 +57,10 @@
 
 (deftest op-00E0
   (println (:doc (meta #'opcode-00E0)))
-  (write-fb 0 0 1 0xFF)
-  (is (= (read-mem 0x80) 19))
+  (write-fb 0 0 [0xFF])
+  (is (= (read-fb 0 0) (unchecked-byte 0xFF)))
   (evaluate 0x00E0)
-  (is (= (read-mem 0x80) 0)))
+  (is (= (read-fb 0 0) 0)))
 
 (deftest op-1nnn
   (println (:doc (meta #'opcode-1nnn)))

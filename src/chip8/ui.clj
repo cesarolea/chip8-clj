ae2f2d8075ba1b659f8fe6206ea698d93935a4b1
César Olea <cesarolea@gmail.com>
2018-09-24 10:39:21 -0700

    Changed aspect ratio

--- src/chip8/ui.clj
+++ src/chip8/ui.clj
@@ -4,7 +4,7 @@
             [mount.core :refer [defstate]]))
 
 (defstate screen
-  :start (let [the-screen (s/get-screen :swing {:cols 192 :rows 32 :font "Times New Roman"})]
+  :start (let [the-screen (s/get-screen :swing {:cols 256 :rows 32 :font "Times New Roman"})]
            (s/start the-screen)
            the-screen)
   :stop (s/stop screen))
@@ -29,8 +29,8 @@
         (loop [x 0]
           (when (<= x 63)
             (when (= 1 (nth row x)))
-            (doseq [x-offset (range 0 3)]
-              (s/put-string screen (+ (* x 3) x-offset) y " " {:bg (if (= (nth row x) 1) :white :black)}))
+            (doseq [x-offset (range 0 4)]
+              (s/put-string screen (+ (* x 4) x-offset) y " " {:bg (if (= (nth row x) 1) :white :black)}))
             (recur (inc x)))))
       (recur (inc y))))
   (s/redraw screen))
